04241d48d56176d8d77656ce4d5e3bdbb22d71bf
Victor <vgil554@gmail.com>
2025-05-31 01:01:39 +0200

    ajout du US 9

--- src/Controller/CarSharingController.php
+++ src/Controller/CarSharingController.php
@@ -3,12 +3,58 @@
 namespace App\Controller;
 
 use App\Entity\CarSharings;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\CarSharingType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class CarSharingController extends AbstractController
 {
+    #[Route('/trajet/nouveau', name: 'app_car_sharing_new')]
+    #[IsGranted('ROLE_USER')] // accessible uniquement aux utilisateurs connectés
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        /** @var \App\Entity\User $user */
+
+        // Vérifier que le chauffeur a assez de crédits
+        if ($user->getCredit() < 2) {
+            $this->addFlash('danger', 'Vous n’avez pas assez de crédits pour créer un trajet.');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $carSharing = new CarSharings();
+        $form = $this->createForm(CarSharingType::class, $carSharing);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $carSharing->setUser($user);
+            $carSharing->setCreatedAt(new \DateTime());
+            $carSharing->setUpdatedAt(new \DateTime());
+            $carSharing->setAvailablePlaces($carSharing->getTotalPlaces());
+            $carSharing->setEcoFriendly(
+                $carSharing->getVehicle()->getEnergy()->getName() === 'Électrique'
+            );
+
+            // Retirer 2 crédits au chauffeur
+            $user->setCredit($user->getCredit() - 2);
+
+            $em->persist($carSharing);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre trajet a bien été créé 🎉');
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('car_sharing/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/trajet/{id}', name: 'app_trajet_show')]
     public function show(CarSharings $carSharing): Response
     {
---
+++ src/Form/CarSharingType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CarSharings;
+use App\Entity\Vehicles;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CarSharingType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('start_address', TextType::class, [
+        'label' => 'Adresse de départ'
+      ])
+      ->add('end_address', TextType::class, [
+        'label' => 'Adresse d’arrivée'
+      ])
+      ->add('date_start', DateTimeType::class, [
+        'label' => 'Date/heure de départ',
+        'widget' => 'single_text',
+      ])
+      ->add('date_end', DateTimeType::class, [
+        'label' => 'Date/heure d’arrivée',
+        'widget' => 'single_text',
+      ])
+      ->add('price', MoneyType::class, [
+        'label' => 'Prix du trajet (€)',
+        'currency' => 'EUR'
+      ])
+      ->add('total_places', IntegerType::class, [
+        'label' => 'Nombre total de places'
+      ])
+      ->add('vehicle', EntityType::class, [
+        'label' => 'Véhicule utilisé',
+        'class' => Vehicles::class,
+        'choice_label' => function ($vehicle) {
+          return $vehicle->getBrand() . ' ' . $vehicle->getModel();
+        },
+        'placeholder' => 'Sélectionnez un véhicule',
+        'required' => true
+      ]);
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => CarSharings::class,
+    ]);
+  }
+}
